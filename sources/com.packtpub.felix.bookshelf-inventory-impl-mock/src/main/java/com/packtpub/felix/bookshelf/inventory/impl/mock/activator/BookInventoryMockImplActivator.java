8a8df5aca052aa71b7fe1c0dd72cba5bb1f70325
theeranitp <theeranitp@gmail.com>
2016-09-04 09:56:13 +0700

    add book inventory impl bundle activator

---
+++ sources/com.packtpub.felix.bookshelf-inventory-impl-mock/src/main/java/com/packtpub/felix/bookshelf/inventory/impl/mock/activator/BookInventoryMockImplActivator.java
@@ -0,0 +1,26 @@
+package com.packtpub.felix.bookshelf.inventory.impl.mock.activator;
+
+import com.packpub.felix.bookshelf.inventory.api.BookInventory;
+import com.packtpub.felix.bookshelf.inventory.impl.mock.BookInventoryMockImpl;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class BookInventoryMockImplActivator implements BundleActivator {
+    private ServiceRegistration reg = null;
+
+    public void start(BundleContext context) throws Exception {
+        System.out.println("\nStarting Book Inventory Mock Impl");
+        this.reg = context.registerService(BookInventory.class.getName(),
+                new BookInventoryMockImpl(), null);
+    }
+
+    public void stop(BundleContext context) throws Exception {
+
+        System.out.println("\nStopping Book Inventory Mock Impl");
+        if (this.reg != null) {
+            context.ungetService(reg.getReference());
+            this.reg = null;
+        }
+    }
+}
